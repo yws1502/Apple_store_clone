7de48fcb037711fe6c8573c174130daa10da7174
Woosang <yws1502@naver.com>
2022-02-08 16:33:12 +0900

    ✨ clayful 연동

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
+import clayful from 'clayful/client-js';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+clayful.config({
+  clinet:
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjQzOTAwZTRhMzViNDg0MTA5MjdhYWM2ZDFmNjA1NWIzNzkyYWUzYjU1YzFhMTc2NjE3Yjk1MTYzYjhiY2Q4YmUiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjQ0MzA0MDY3LCJzdG9yZSI6IlM3RUxaNFE5RTlMVy5CUEs2OExYREpaVFYiLCJzdWIiOiI1NEdaM1ZYSjZFVEoifQ.eraf9O-Ox5M7fAkDkxbGEaI_7lcaUTRKhu4SQxaRJA8",
+});
+
+clayful.install("request", require("clayful/plugins/request-axios")(axios));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,31 @@
 import React from "react";
+import clayful from "clayful/client-js";
 
 function LandingPage () {
+  const Product = clayful.Product;
+
+  const options = {
+    query: {
+      page: 1
+    }
+  };
+
+  Product.list(options, function (err, res) {
+    if (err) {
+      console.log(err.isClayful);
+      console.log(err.model);
+      console.log(err.method);
+      console.log(err.status);
+      console.log(err.headers);
+      console.log(err.code);
+      console.log(err.message);
+    }
+    
+    console.log(res.status);
+    console.log(res.headers);
+    console.log(res.data);
+  })
+
   return (
     <div>
       LandingPage
